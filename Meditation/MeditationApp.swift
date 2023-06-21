fa8a77f7d7cb65eb2185669c005e330588c7b951
MsRapunzel <anastasya.zolotova11@gmail.com>
2023-06-21 22:17:50 +0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Anastasiia Zolotova on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
