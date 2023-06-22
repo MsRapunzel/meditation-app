d39a51750193e3bddeebc9d56bf85ae13c78f8c7
MsRapunzel <anastasya.zolotova11@gmail.com>
2023-06-22 13:54:19 +0300

    Getting Started

---
+++ Meditation/MeditationView.swift
@@ -0,0 +1,21 @@
+//
+//  MeditationView.swift
+//  Meditation
+//
+//  Created by Anastasiia Zolotova on 22.06.2023.
+//
+
+import SwiftUI
+
+struct MeditationView: View {
+    var body: some View {
+        VStack() {
+        }
+    }
+}
+
+struct MeditationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeditationView()
+    }
+}
